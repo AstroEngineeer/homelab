f6744f6f698248facfdaf48b14f57b176214aac1
batman <batman@xerogravity.space>
2023-11-06 17:50:55 +0530

    Added mkdir for all docker bind volume and files

---
+++ docker-volumes.sh
@@ -0,0 +1,8 @@
+mkdir chilflix-backend/bazarr-config chilflix-backend/prowlaar-config chilflix-backend/qbittorrent-config chilflix-backend/radarr-config chilflix-backend/sonarr-config 
+mkdir chilflix-frontend/jellyfin-cache chilflix-frontend/jellyfin-config chilflix-frontend/jellyseerr-config
+touch cloudflared/.env
+mkdir dashboard/homarr-configs dashboard/homarr-icons
+mkdir dns/bind9
+touch dns/bind9-config/named.conf.options dns/bind9-config/db.domain
+mkdir vpn/gluetun-config
+touch vpn/.env
