f44df09c8644ba561dd541d031a6633d8b308016
batman <batman@xerogravity.space>
2023-11-08 23:32:19 +0530

    added traefik

--- docker-networks.sh
+++ docker-networks.sh
@@ -6,18 +6,16 @@ docker network create \
 
 docker network create \
     --driver=bridge \
-    --gateway=172.19.0.1 \
-    --subnet=172.19.0.0/16 \
     chilflix_frontend
 
 docker network create \
     --driver=bridge \
-    --gateway=172.20.0.1 \
-    --subnet=172.20.0.0/16 \
     dashboard
 
 docker network create \
     --driver=bridge \
-    --gateway=172.21.0.1 \
-    --subnet=172.21.0.0/16 \
     dns
+
+docker network create \
+    --driver=bridge \
+    proxy
