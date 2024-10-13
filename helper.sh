7121c30db480cac8884718a9c584b82a175e3d38
neo@matrix <58505455+vigneshganesan008@users.noreply.github.com>
2024-10-13 13:45:36 +0530

    removed monitoring network from helper script

--- helper.sh
+++ helper.sh
@@ -3,9 +3,3 @@ docker network create \
     --gateway=172.20.0.1 \
     --subnet=172.20.0.0/16 \
     proxy
-
-docker network create \
-    --driver=bridge \
-    --gateway=172.19.0.1 \
-    --subnet=172.19.0.0/16 \
-    monitoring
