8302f30102d4b3220f6ca199b2fa39d2b46b9a41
neo@matrix <58505455+vigneshganesan008@users.noreply.github.com>
2024-11-30 12:29:50 +0530

    Removed exposed ports + added tsdporxy support

--- helper.sh
+++ helper.sh
@@ -1,5 +1,11 @@
 docker network create \
     --driver=bridge \
-    --gateway=172.20.0.1 \
-    --subnet=172.20.0.0/16 \
-    proxy
+    --gateway=172.18.0.1 \
+    --subnet=172.18.0.0/16 \
+    tk-proxy
+
+docker network create \
+    --driver=bridge \
+    --gateway=172.19.0.1 \
+    --subnet=172.19.0.0/16 \
+    ts-proxy
